a9d5a358ed9e95c74fc9d34f7e86f27258766950
Kazim Hussain <77475849+Kazim-H@users.noreply.github.com>
2024-01-27 13:59:43 +0500

    Add files via upload

---
+++ python_course/Patterns.py
@@ -0,0 +1,78 @@
+# 5 5 5 5 5
+# 5 5 5 5
+# 5 5 5
+# 5 5
+# 5
+# for i in range(5,0, -1):
+#         b = 5
+#         for n in range(1, i + 1):
+#             print(b, end=' ')
+#         print(" ")
+
+# 5 5 5 5 5
+# 4 4 4 4
+# 3 3 3
+# 2 2
+# 1
+# for i in range(5,0,-1):
+#     n= i
+#     for o in range(0,i):
+#         print(n, end=' ')
+#     print(" ")
+
+# 1
+# 3 3
+# 5 5 5
+# 7 7 7 7
+# 9 9 9 9 9
+# for i in range(1, 5+1):
+#     for o in range(i):
+#         print((i * 2 - 1), end=" ")
+#     print()
+
+# 1
+# 2 1
+# 3 2 1
+# 4 3 2 1
+# 5 4 3 2 1
+# for i in range(1,6):
+#     for n in range(i, 0, -1):
+#         print(n, end=' ')
+#     print(" ")
+
+
+
+# * * * * *
+#  * * * *
+#   * * *
+#    * *
+#     *
+#    * *
+#   * * *
+#  * * * *
+# * * * * *
+# for i in range(5):
+#     print(" " * i, end="")
+#     print("* " * (5 - i))
+# for i in range(5 - 2, -1, -1):
+#     print(" " * i, end="")
+#     print("* " * (5 - i))
+
+
+# DRAFT BELOW
+# num = 1
+# for r in range(1, 5):
+#     for c in range(r,):
+#         print(num, end=' ')
+#         num += 1
+#     print()
+
+
+# counter = 0
+# for i in range(1,8,2):
+#     counter = counter + 1
+#     for j in range(0,counter):
+#         print(i-j,end = " ")
+#     print()
+
+
