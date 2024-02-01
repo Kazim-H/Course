d5f4263c19f7441ce2e023a0f244acba081a1a3f
Kazim Hussain <77475849+Kazim-H@users.noreply.github.com>
2024-02-01 23:07:49 +0500

    Add files via upload

---
+++ python_course/String Functions.py
@@ -0,0 +1,41 @@
+# # Create a list of words.
+# input1 = input("Enter some words: ")
+# word_list = input1.split()
+# print("List of words:", word_list)
+
+# # Create a string from the list.
+# _list = ["checking"]
+# _string = ' '.join(_list)
+# print("String from the list of words:", _string)
+
+# #Capitalize the first letter of the entire string. Print the modified string.
+# _input=input("Enter some words: ")
+# _string=_input
+# _string= _string.capitalize()
+# print("Modified string:", _string)
+
+# # find and print the index of the first occurrence of the letter 'a' in the string.
+# _input = input("Enter a sentence or a series of words: ")
+# index_ =_input.find('a')
+# print("The letter 'a' occurs at index", index_, "first time")
+
+# #count and print the number of occurrences of the letter 'a'.
+# _input = input("Enter a word: ")
+# count_of_a = _input.count('a')
+# print("The letter 'a' occurs", count_of_a, "times in", _input )
+
+# # replace 'a' with 'x'. Print the modified string.
+# _input = input("Enter a sentence or a series of words: ")
+# _string = _input.replace('a', 'x')
+# print("Modified string:", _string)
+
+# # remove leading and trailing whitespaces. Print the modified string
+# _input = input("Enter a sentence: ")
+# _string = _input.strip()
+# print("Modified string:", _string)
+
+# # split the string back into a list. Print the resulting list.
+# user_input = input("Enter a sentence: ")
+# resulting_list = user_input.split()
+# print("Resulting list:", resulting_list)
+
