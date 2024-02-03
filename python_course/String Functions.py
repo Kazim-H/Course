5d0a804a7966dcc0bc249a6bf517a0a58b658dc4
Kazim Hussain <77475849+Kazim-H@users.noreply.github.com>
2024-02-03 16:18:58 +0500

    Update String Functions.py

--- python_course/String Functions.py
+++ python_course/String Functions.py
@@ -1,41 +1,32 @@
+word_list = ["apple", "banana", "cherry", "date"]
 # Create a list of words.
-input1 = input("Enter some words: ")
-word_list = input1.split()
-print("List of words:", word_list)
+print("List:", word_list)
 
 # Create a string from the list.
-_list = ["checking"]
-_string = ' '.join(_list)
-print("String from the list of words:", _string)
+_string = ' '.join(word_list)
+print("String:", _string)
 
-#Capitalize the first letter of the entire string. Print the modified string.
-_input=input("Enter some words: ")
-_string=_input
-_string= _string.capitalize()
-print("Modified string:", _string)
+# Capitalize the first letter of the entire string. Print the modified string.
+modified_string = _string.capitalize()
+print("Capitalize the first letter:", modified_string)
 
-# find and print the index of the first occurrence of the letter 'a' in the string.
-_input = input("Enter a sentence or a series of words: ")
-index_ =_input.find('a')
+# Find and print the index of the first occurrence of the letter 'a' in the string.
+index_ = _string.find('a')
 print("The letter 'a' occurs at index", index_, "first time")
 
-#count and print the number of occurrences of the letter 'a'.
-_input = input("Enter a word: ")
-count_of_a = _input.count('a')
-print("The letter 'a' occurs", count_of_a, "times in", _input )
+# Count and print the number of occurrences of the letter 'a'.
+count_of_a = _string.count('a')
+print("The letter 'a' occurs", count_of_a, "times in", _string)
 
-# replace 'a' with 'x'. Print the modified string.
-_input = input("Enter a sentence or a series of words: ")
-_string = _input.replace('a', 'x')
-print("Modified string:", _string)
+# Replace 'a' with 'x'. Print the modified string.
+modified_string = _string.replace('a', 'x')
+print("Replaced 'a' with 'x':", modified_string)
 
-# remove leading and trailing whitespaces. Print the modified string
-_input = input("Enter a sentence: ")
-_string = _input.strip()
-print("Modified string:", _string)
+# Remove leading and trailing whitespaces. Print the modified string.
+_input = " " + _string + " "
+modified_string = _input.strip()
+print("Remove leading and trailing whitespaces:", modified_string)
 
-# split the string back into a list. Print the resulting list.
-user_input = input("Enter a sentence: ")
-resulting_list = user_input.split()
+# Split the string back into a list. Print the resulting list.
+resulting_list = _string.split()
 print("Resulting list:", resulting_list)
-
